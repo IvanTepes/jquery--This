d646c21f77e1ebe77d27c186e846d4b14fcf0d9e
IvanTepes <ivan.tepes@outlook.com>
2020-07-02 17:03:16 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* $('p').click(function() {
+    $(this).slideToggle('slow');
+}); */
+
+/* $('button').mouseenter(function(){
+    $(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseleave(function(){
+    $(this).removeClass('makeBlue').addClass('makeRed');
+});
+ */
+
+ $(document).ready(function(){
+     $(".box").on("click", function(){
+         let classNames = $(this).attr("class").split(" ");
+         let boxClass = classNames[0];
+         let className = classNames[1];
+         if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+             $("." + className).css("background-color", "#000");
+         } else {
+             $("." + boxClass).css("background-color", "#000");
+             $("." + className).css("background-color", "red");
+         };
+     });
+ });
